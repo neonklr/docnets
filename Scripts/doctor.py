18206f496652eef45c6c5b099e405d9fedbb4b63
drgn0 <70508632+drgn0@users.noreply.github.com>
2022-05-01 19:09:33 +0530

    Add files via upload

--- Scripts/doctor.py
+++ Scripts/doctor.py
@@ -1,14 +1,50 @@
 import Scripts.data_loader as data_loader
 import Scripts.data_saver as data_saver 
 
+# import data_loader 
+# import data_saver 
+
 
 appointment_data = None 
+doctors_data = None 
 
-def update_data():
+def reload_appointment_data():
     global appointment_data 
     appointment_data = data_loader.get_appointment_data() 
 
+def reload_doctors_data():
+    global doctors_data 
+    doctors_data = data_loader.get_doctors_data() 
+
+
+
+def get_nearest_docs(loc):
+    reload_doctors_data() 
+
+    return sorted(
+        doctors_data.keys(), 
+        key = lambda doc_id: distance2(
+            doctors_data[doc_id]["location"], loc
+        )
+    ) 
+
+def distance2(x, y):
+    return (x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2   
+
+
+def get_docs_sorted_by(criteria):
+    assert(criteria in ["fees", "experience", "rating"])
+    reload_doctors_data() 
+
+    return sorted(
+        doctors_data.keys(), 
+        key = lambda doc_id: doctors_data[doc_id][criteria], 
+        reverse = criteria != "fees"
+    )
+
 def get_docs_availiable_at(date_and_time, check_free = True):     
+    reload_appointment_data() 
+
     for doc_id in appointment_data.keys():
         if is_availiable(doc_id, date_and_time):
             if check_free and not has_appointment(doc_id, date_and_time):
@@ -40,21 +76,39 @@ def has_appointment(doc_id, date_and_time):
     return date_and_time.time() not in schedule[date_and_time.date()] 
 
 
+
+
 def book(date_and_time, doc_id, patient_id):
     doc_id = str(doc_id)
 
-    check_if_booking_is_valid()     
+    assert(is_future_date(date_and_time)) 
+    assert(check_if_booking_is_valid(doc_id, date_and_time))
+
     data_saver.add_booking(date_and_time, doc_id, patient_id) 
 
 def check_if_booking_is_valid(doc_id, date_and_time):
-    update_data() 
-    assert(has_appointment(doc_id, date_and_time))
+    reload_appointment_data() 
+    return has_appointment(doc_id, date_and_time)
+
+def is_future_date(date_and_time):
+    from datetime import datetime 
+    
+    return datetime.now() <= date_and_time 
+
+
 
 
 if __name__ == '__main__':
     import datetime 
-    date_and_time = datetime.datetime(2022, 5, 1, 15)  
 
-    # print('Availiable Docs:', *get_docs_availiable_at(date_and_time)) 
-    book(date_and_time, 1, 1011)
+    for c in ["fees", "experience", "rating"]:
+        print(c, get_docs_sorted_by(c))
+    # date_times = [datetime.datetime(2022, 5, 2, 14), datetime.datetime(2022, 5, 3, 10)]
+
+    # for date_and_time in date_times:
+    #     print('Availiable Docs:', *get_docs_availiable_at(date_and_time)) 
+    #     book(date_and_time, 2, 1013)
+
+
+    # print(get_nearest_docs((400, 400)))
 
