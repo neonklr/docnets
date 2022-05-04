ecb9ae28640c05f3eaa57579814064ec6a199585
Rishaab Kalra <55053464+PeaPals@users.noreply.github.com>
2022-05-04 17:10:44 +0530

    Minor Bugs Fix

--- Pages/home_page.py
+++ Pages/home_page.py
@@ -34,7 +34,7 @@ def second_header_util(container, icon, heading, content):
 def second_header(st):
     Utils.website_heading(
         st,
-        content="DocNets, An easy-to-use appointment scheduling app for doctors",
+        content="DocNets, An easy-to-use appointment scheduling app for a patient with a doctor",
         symbol=".",
         font_size=60,
         color="green",
